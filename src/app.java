c75cb777568d5c428b3cb43efb8b6bbfff458785
talesmelo <talesbmelo@gmail.com>
2023-05-18 18:09:08 -0300

    criação da atividade do nome e idade

--- src/app.java
+++ src/app.java
@@ -1,5 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Sistema na versão Moderna/criacao/nome-idade");
+        System.out.println("Nome: Tales Melo Idade: 36 anos");
     }
 }
