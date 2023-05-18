1cf196b3bc6d7c88dba5251e99017702a82fd313
talesmelo <talesbmelo@gmail.com>
2023-05-18 17:19:18 -0300

    segundo commit

--- src/app.java
+++ src/app.java
@@ -2,5 +2,6 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Olá mundo no GITHUB versao 2");
         var x = "teste";
+        var y = "teste2";
     }
 }
