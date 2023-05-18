b335709ac2b2627042aa7dfe5b06991d104a2109
talesmelo <talesbmelo@gmail.com>
2023-05-18 17:39:38 -0300

    versão 3.0

--- src/app.java
+++ src/app.java
@@ -1,8 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Olá mundo no GITHUB versao 2");
-        var x = "teste";
-        var y = "teste2";
-        var v = "teste3";
+        System.out.println("Olá mundo no GITHUB versao 3.0");
     }
 }
